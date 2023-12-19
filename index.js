10dac91da37b7311dbc2463e9a328fb75d099b43
Benny Wick <bwick@Bennys-MBP.home>
2023-12-18 16:14:32 -0800

    push code

---
+++ index.js
@@ -0,0 +1,42 @@
+let passwordOneEl = document.getElementById('password-one-text')
+let passwordTwoEl = document.getElementById('password-two-text')
+let tooltip1El = document.getElementById('tooltip-1')
+let tooltip2El = document.getElementById('tooltip-2')
+
+function generatePasswords() {
+    const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
+    "V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
+    "u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%",
+    "^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+    let passwordLength = 15;
+    let passwordOne = "";
+    let passwordTwo = "";
+    
+    for (let i = 0; i < passwordLength; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        passwordOne += characters[randomIndex]
+    }
+
+    for (let i = 0; i < passwordLength; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length)
+        passwordTwo += characters[randomIndex]
+    }
+
+    tooltip1El.textContent = 'Copy to clipboard'
+    tooltip2El.textContent = 'Copy to clipboard'
+    passwordOneEl.textContent = passwordOne
+    passwordTwoEl.textContent = passwordTwo
+}
+
+function copyToClipboard(el) {
+    let copyText = document.getElementById(el).textContent.trim()
+    navigator.clipboard.writeText(copyText)
+    console.log(copyText)
+    if (el == "password-one-btn") {
+        tooltip1El.textContent = 'Copied!'
+        tooltip2El.textContent = 'Copy to clipboard'
+    } else {
+        tooltip2El.textContent = 'Copied!'
+        tooltip1El.textContent = 'Copy to clipboard'
+    }
+}
